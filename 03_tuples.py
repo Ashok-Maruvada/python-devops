2668972df27132e91b6eb52b812afee35bdf0a71
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-07 13:12:33 +0530

    tuple

---
+++ 03_tuples.py
@@ -0,0 +1,9 @@
+# There are 2 methods to create a tuple
+# 1. ()
+# 2. tuple()
+# Behavior: They're immutable
+
+sample_tuple=('jira','ansible','terraform','shell','jenkins')
+
+element=sample_tuple[1]
+print(element)
