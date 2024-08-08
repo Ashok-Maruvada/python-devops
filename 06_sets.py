706a0fc745a30010966061236582932c8367d83b
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-08 14:43:17 +0530

    sets

---
+++ 06_sets.py
@@ -0,0 +1,5 @@
+# A Set returns unique elements that is stored inside that variable
+# Sets don't consider the order of insertion
+# Sets don't support indexing
+sample_set={1,2,3,6,3,5}
+print(sample_set)
