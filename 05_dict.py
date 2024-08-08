f3f344bdc54f37273a691d0ed1f0340ace47ebbe
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-08 13:30:51 +0530

    test

--- 05_dict.py
+++ 05_dict.py
@@ -2,3 +2,12 @@
 #dict is mutable but keys are immutable
 sample_dict= {1:1,2:4,3:9}
 print(sample_dict)
+
+sample_dict = {1: 1, 2: 4, 3: 9, 3: 15}
+print(sample_dict[3])
+
+sample_dict = {(1, 2, 3, 4): 1, 2: 4, 3: 9}
+print(sample_dict[(1, 2, 3, 4)])
+
+sample_dict = {"1": 1, 2: 4, 3: 9}
+print(sample_dict["1"])
