d5a0b4fc58bde7f93ae4114078f58285e1b24b3b
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-08 13:36:23 +0530

    types

--- 05_dict.py
+++ 05_dict.py
@@ -1,13 +1,17 @@
 #dictionary is like a key-value pair
-#dict is mutable but keys are immutable
+#dict is mutable data type but keys are immutable
 sample_dict= {1:1,2:4,3:9}
 print(sample_dict)
+print(sample_dict[3])
 
 sample_dict = {1: 1, 2: 4, 3: 9, 3: 15}
-print(sample_dict[3])
+print(sample_dict[3]) # gives latest value i.e 15
 
 sample_dict = {(1, 2, 3, 4): 1, 2: 4, 3: 9}
 print(sample_dict[(1, 2, 3, 4)])
 
 sample_dict = {"1": 1, 2: 4, 3: 9}
 print(sample_dict["1"])
+
+sample_dict = {[1, 2, 3, 4]: 1, 2: 4, 3: 9}
+print(sample_dict[[1, 2, 3, 4]])
