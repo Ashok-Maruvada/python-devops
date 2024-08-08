839059582e5f33e38caec993f5f3c972d047caee
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-08 14:58:00 +0530

    str to tuple

--- 07_type_conversion.py
+++ 07_type_conversion.py
@@ -1,3 +1,7 @@
 sample_str="this is a string"
+
 sample_list=list(sample_str)
-print(sample_list)
+print(sample_list) # ['t', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g']
+
+sample_tuple=tuple(sample_str)
+print(sample_tuple)
