c6afb982e1d46940ec43c3634dab25bef92e4138
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-08 14:49:11 +0530

    add

--- 06_sets.py
+++ 06_sets.py
@@ -6,5 +6,15 @@ print(sample_set)
 
 # Add elements to a set
 sample_set={1,2,3,6,3,5}
+"""
 sample_set[1]=10
+print(sample_set)
+
+Traceback (most recent call last):
+  File "/home/cloudshell-user/python-devops/06_sets.py", line 9, in <module>
+    sample_set[1]=10
+TypeError: 'set' object does not support item assignment
+
+"""
+sample_set.add(4)
 print(sample_set)
