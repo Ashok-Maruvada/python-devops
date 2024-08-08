fb661c434910a2bce64e3231e7fe489d48efdf80
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-08 15:00:58 +0530

    input

--- 07_type_conversion.py
+++ 07_type_conversion.py
@@ -4,4 +4,8 @@ sample_list=list(sample_str)
 print(sample_list) # ['t', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g']
 
 sample_tuple=tuple(sample_str)
-print(sample_tuple)
+print(sample_tuple) # ('t', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g')
+
+# Accept input from a user
+user_input=input()
+print(user_input)
