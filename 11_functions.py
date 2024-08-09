ef68a01047321521ffc4410e6f3472db3e57ab9b
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 17:09:19 +0530

    lambda

--- 11_functions.py
+++ 11_functions.py
@@ -39,8 +39,8 @@
 # addition=add(1,2,3,4,5)
 # print(addition)
 
-def add(*args, **kwargs):
-    print(args, kwargs)
+# def add(*args, **kwargs):
+#     print(args, kwargs)
 
 #res = add(1, 2, 4)
 #print(res) # o/p is None as we are not return anything
@@ -50,8 +50,15 @@ def add(*args, **kwargs):
 None
 """
 
-res=add(1,2,3,num1=5,num2=7)
-print(res)
+# res=add(1,2,3,num1=5,num2=7)
+# print(res)
+"""
+[cloudshell-user@ip-10-134-37-110 python-devops]$ python 11_functions.py 
+(1, 2, 3) {'num1': 5, 'num2': 7}
+None
 """
 
-"""
+# Lambda: inline function
+add_numbers = lambda num1,num2: num1+num2
+res= add_numbers(1,2)
+print(res)
