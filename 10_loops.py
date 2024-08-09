cd77fab684a101d42e0d52d336427b58906b35fa
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 15:36:46 +0530

    test

--- 10_loops.py
+++ 10_loops.py
@@ -1,16 +1,16 @@
 sample_list=['k8','ansible','shell','jenkins']
 
-for i in sample_list:
-    print(i, len(i))
+# for i in sample_list:
+#     print(i, len(i))
 
-# Print the element and its corresponding index
-for i,idx in enumerate(sample_list):
-    print(i,idx)
+# # Print the element and its corresponding index
+# for i,idx in enumerate(sample_list):
+#     print(i,idx)
 
 # Range based for loop
 sample_range= range(0, len(sample_list))
-print(sample_list, type(sample_range))
+#print(sample_list, type(sample_range))
 
-for i in sample_range:
-    print(i, sample_range)
+for idx in range(0, len(sample_list)):
+    print(idx, sample_list[idx])
                     
