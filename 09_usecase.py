56fc562135a2fb649d862496760b3e37ac4ee63d
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 15:13:30 +0530

    usecase

--- 09_usecase.py
+++ 09_usecase.py
@@ -3,8 +3,8 @@
 username="Ashok"
 password="DevOps321"
 
-UserName=input("please enter username")
-PassWord=input("please enter password")
+UserName=input("please enter username: ")
+PassWord=input("please enter password: ")
 
 if (UserName == username) & (PassWord == password):
     print("login is successful")
