4a967b7ffe89d10bbcf9149809666082d7614e63
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 13:50:37 +0530

    nested if else

--- 08_conditional.py
+++ 08_conditional.py
@@ -2,7 +2,18 @@
 
 user_input=int(input("enter a number:"))
 
+# if user_input>10:
+#     print("given number is greater than 10")
+# else:
+#     print("given number is less than or equal to 10")
+
+# Check whether the given number is equal to 10 or greater than 10 or less than 10 or something else
+
+# Method 1: Nested if-else
 if user_input>10:
     print("given number is greater than 10")
 else:
-    print("given number is less than or equal to 10")
+    if user_input<10:
+        print("given number is less than 10")
+    else:
+        print("given number is equal to 10")
