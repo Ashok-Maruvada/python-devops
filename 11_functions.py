455e42cd57378dad6fcde573bc66f5af4f20522f
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 16:30:45 +0530

    test

--- 11_functions.py
+++ 11_functions.py
@@ -1,13 +1,12 @@
 # def sample_func():
 #     print("this is sample function")
-
 # #calling the function
 # sample_func()
 
 def add(num1, num2):
     res=num1+num2
     return res
-
 # calling the function
-addition= add(1,2)
+#addition= add(1,2)
+addition=add(num1=2,num2=3)
 print(addition)
