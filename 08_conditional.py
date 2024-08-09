ceaa173082c2250f8e221919a436c23b1b044810
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 13:45:27 +0530

    conditions

---
+++ 08_conditional.py
@@ -0,0 +1,8 @@
+# Check whether the given number is greater than 10 or not
+
+user_input=int(input("enter a number:"))
+
+if user_input>10:
+    print("given number is greater than 10")
+else:
+    print("given number is less than or equal to 10")
