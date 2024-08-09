588109c306ffc87329df346cdb8d71c095bb6243
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 13:59:55 +0530

    exception handling

--- 08_conditional.py
+++ 08_conditional.py
@@ -10,18 +10,30 @@ user_input=int(input("enter a number:"))
 # Check whether the given number is equal to 10 or greater than 10 or less than 10 or something else
 
 # Method 1: Nested if-else
-if user_input>10:
-    print("given number is greater than 10")
-else:
-    if user_input<10:
-        print("given number is less than 10")
-    else:
-        print("given number is equal to 10")
+# if user_input>10:
+#     print("given number is greater than 10")
+# else:
+#     if user_input<10:
+#         print("given number is less than 10")
+#     else:
+#         print("given number is equal to 10")
 
-# Method 2
-if user_input==10:
-    print("gievn number is equal to 10")
-elif user_input>10:
-    print("given number is greater than 10")
-else:
-    print("given number is less than 10")
+# # Method 2
+# if user_input==10:
+#     print("gievn number is equal to 10")
+# elif user_input>10:
+#     print("given number is greater than 10")
+# else:
+#     print("given number is less than 10")
+
+# Exception handling: try - except
+try:
+    user_input=int(input("enter a number:"))
+    if user_input==10:
+        print("gievn number is equal to 10")
+    elif user_input>10:
+        print("given number is greater than 10")
+    else:
+        print("given number is less than 10")
+except SyntaxError:
+    print("Please enter a number ")
