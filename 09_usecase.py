f3da2ad2ccc6842eb311ef5bf452760e798eae3b
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 15:11:34 +0530

    usecase

---
+++ 09_usecase.py
@@ -0,0 +1,14 @@
+# Login System
+
+username="Ashok"
+password="DevOps321"
+
+UserName=input("please enter username")
+PassWord=input("please enter password")
+
+if (UserName == username) & (PassWord == password):
+    print("login is successful")
+else:
+    print("invalid credentials")
+               
+
