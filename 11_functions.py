7ddb5da23248d305faa1994c73eade8de087cf32
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 16:33:16 +0530

    add

--- 11_functions.py
+++ 11_functions.py
@@ -10,3 +10,12 @@ def add(num1, num2):
 #addition= add(1,2)
 addition=add(num1=2,num2=3)
 print(addition)
+
+
+def add(num1,num2,num3=1):
+    res=num1+num2+num3
+    return res
+
+# call the function
+addition=add(2,3)
+print(addition)
