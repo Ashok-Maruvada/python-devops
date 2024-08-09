1e7accb12f418b8a6a5518b5db3e16227352b3e2
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-08-09 16:39:52 +0530

    test

--- 11_functions.py
+++ 11_functions.py
@@ -12,11 +12,21 @@
 # print(addition)
 
 
-def add(num1,num2,num3=1):
-    res=num1+num2+num3
+# def add(num1,num2,num3=1):
+#     res=num1+num2+num3
+#     return res
+
+# # call the function
+# #addition=add(2,3)
+# addition=add(num2=5,num1=4)
+# print(addition)
+
+# One user enters 10 numbers and another user enters 100 numbers. Define your function to handle this situation
+# A: Variable length arguments
+def add(*nums):
+    res=sum(nums)
     return res
 
-# call the function
-#addition=add(2,3)
-addition=add(num2=5,num1=4)
+#calling the function
+addition=add(1,2,3,4)
 print(addition)
